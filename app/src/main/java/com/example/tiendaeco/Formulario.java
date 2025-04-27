3a944417e4a49564bfde3a8f6c7a7d1536c86712
Carlos Sosa <cmsosa@ucompensar.edu.co>
2025-04-26 20:08:48 -0500

    Conexion de home a formulario

---
+++ app/src/main/java/com/example/tiendaeco/Formulario.java
@@ -0,0 +1,13 @@
+package com.example.tiendaeco;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Formulario extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acttivity_formulario);
+    }
+}
--- app/src/main/java/com/example/tiendaeco/HomeActivity.java
+++ app/src/main/java/com/example/tiendaeco/HomeActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,5 +19,19 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        Button btnRegister = findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, Formulario.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
+
+
 }
