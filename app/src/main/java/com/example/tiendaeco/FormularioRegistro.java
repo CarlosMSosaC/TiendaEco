c2431b78653516b1477a28d9852659179d8b7ceb
Carlos Sosa <cmsosa@ucompensar.edu.co>
2025-04-27 00:46:45 -0500

    Carta Productos y Funcionalidad Botones

--- app/src/main/java/com/example/tiendaeco/FormularioRegistro.java
+++ app/src/main/java/com/example/tiendaeco/FormularioRegistro.java
@@ -1,6 +1,9 @@
 package com.example.tiendaeco;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,15 @@ public class FormularioRegistro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_ingreso);
+
+        Button btnIngresar = findViewById(R.id.btnIngresar);
+
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FormularioRegistro.this, CartaProductos.class);
+                startActivity(intent);
+            }
+        });
     }
 }
